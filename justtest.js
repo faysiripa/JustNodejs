5125ef2fe9f2fff575adf7effa65d2b9ccd7edc3
fay <hisakiryouhei@gmail.com>
2025-09-24 21:19:44 +0700

    2 Async (callback)

--- justtest.js
+++ justtest.js
@@ -2,7 +2,7 @@
 // 3 วิธี => 1 Sync , 2 Async (callback) , 3 Promise async await
 
 const http = require('http');
-require ('dotenv').config();
+require('dotenv').config();
 const { APP_PORT } = process.env;
 const fs = require('fs');
 const path = require('path');
@@ -12,26 +12,41 @@ const server = http.createServer((req, res) => {
 
     console.log(`Start ${url}`);
 
-    try {
-        fs.statSync('logs');
-    }
-    catch(error){
-        fs.mkdirSync('logs');
-    }
+    // วิธีที่ 1 Sync (บล็อกการทำงาน)
+    // try {
+    //     fs.statSync('logs');
+    // }
+    // catch(error){
+    //     fs.mkdirSync('logs');
+    // }
 
+    // const logContent = `${new Date()} : ${method} ${url}\n`;
+    // fs.writeFileSync(path.join('logs','request.log'),logContent, { flag: 'a+'});
+
+    // วิธีที่ 2 Async (callback) (ไม่บล็อกการทำงาน)
     const logContent = `${new Date()} : ${method} ${url}\n`;
-    fs.writeFileSync(path.join('logs','request.log'),logContent, { flag: 'a+'});
-
-    console.log(`End ${url}`);
-
+    fs.stat('logs', (error, stats) => {
+        if (!!error) {
+            fs.mkdir('logs', (error1) => {
+                fs.writeFile(path.join('logs', 'request.log'), logContent, { flag: 'a+' }, (error2) => {
+                    console.log(`End ${url}`);
+                });
+            });
+            return;
+        }
+        fs.writeFile(path.join('logs', 'request.log'), logContent, { flag: 'a+' }, (error2) => {
+            console.log(`End ${url}`);
+        });
+    });
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html; charset=UTF-8');
     res.end('<h1>ล็อกหลบแบบดิจิทัลลลลลลลลล</h1>');
-});
 
+});
 server.listen(APP_PORT, () => {
     console.log('Server is start the war, on port ' + APP_PORT);
 });
 
-// ข้อเสีย ถ้ามีลูกค้าจำนวนมากจะทำให้ server ช้า เพราะการเขียนไฟล์แบบ sync จะ block การทำงาน
-// ข้อดี คือ เขียนง่าย ไม่ต้องยุ่งยากกับ callback หรือ promise
+// ข้อดีของ Async คือ ไม่บล็อกการทำงานของโปรแกรมที่อยู่ข้างล่างถัดไป
+// ข้อเสียของ Async คือ ไม่ได้เรียงลำดับการทำงาน
+// สมมติ เราเขียน log file ช้า (เช่น log file ใหญ่) แล้วมี request เข้ามาพร้อมๆ กัน

93b5e4df0dfa6aca6e0ab8bbda465c74f39e21f9
fay <hisakiryouhei@gmail.com>
2025-09-24 20:07:45 +0700

    #10 Write File

--- justtest.js
+++ justtest.js
@@ -2,26 +2,17 @@ const http = require('http');
 require ('dotenv').config();
 const { APP_PORT } = process.env;
 const fs = require('fs');
-const path = require('path');
-const marked = require('marked');
+const { log } = require('console');
 
 const server = http.createServer((req, res) => {
     const { method, url } = req;
 
-    let content = ''
-    if (method === 'GET' && url === '/') {
-        try {
-            content = fs.readFileSync(path.resolve('files','home.md'), 
-            'utf-8');
-            content = marked.parse(content);
-        } catch (error) {
-            console.error(error);
-        }
-    }
+    const logContent = `${new Date()} : ${method} ${url}\n`;
+    fs.writeFileSync('request.log',logContent, { flag: 'a+'});
 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html; charset=UTF-8');
-    res.end(content);
+    res.end('<h1>ล็อกหลบแบบดิจิทัลลลลลลลลล</h1>');
 });
 
 server.listen(APP_PORT, () => {
